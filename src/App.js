3e5d712b85f09e9e218fef1e936c66c8fc6e199b
Justifall <84130430+Angst04@users.noreply.github.com>
2024-01-07 03:39:06 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Inventory from './components/Inventory';
+import './styles/App.css'
+
+function App() {  
+  return (
+    <div className="App">
+      <Inventory/>
+    </div>
+  )
+}
+
+export default App;
